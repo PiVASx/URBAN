542ad0a07b9e023488ef8dd1abf12a7124b2b540
PiVASx <SergeevVasia85@gmail.com>
2024-08-11 22:12:02 +0300

    Исправление для модуля 2.4

--- module_2_4.py
+++ module_2_4.py
@@ -1,26 +1,18 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 333333333]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = False
 
-for i in numbers:
-    count = 0
-    for j in range(1, i+1):
-        if i % j == 0:
-            count += 1
-            if count > 2:
+for num in numbers:
+    is_prime = True
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
                 break
-    if count < 2:
-        continue
-    elif count == 2:
-        is_prime = True
-    else:
-        is_prime = False
-
-    if is_prime:
-        primes.append(i)
-    else:
-        not_primes.append(i)
+        if is_prime:
+            primes.append(num)
+        else:
+            not_primes.append(num)
 
 print(f'Primes: {primes}')
 print(f'Not Primes: {not_primes}')
