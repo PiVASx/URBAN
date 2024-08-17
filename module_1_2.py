b161a87de2dd179f94a586df592fac72a1ce5098
glpat-BgjeT-FSL-bEXXenhGCN <sergeevvasia85@gmail.com>
2024-08-17 20:52:17 +0300

    refactor fails

--- module_1_2.py
+++
@@ -1,6 +0,0 @@
-cont_true_dz = 12
-cont_hour = 1.5
-name_kurses = 'Python'
-time = cont_hour / cont_true_dz
-
-print(f'Курс: {name_kurses}, всего задач: {cont_true_dz}, затрачено часов: {cont_hour}, среднее время выполнения {time} часа.')
--- module_1_3.py
+++
@@ -1,8 +0,0 @@
-name = 'Vasia'
-print(f'Name: {name}')
-age = 38
-print(f'Age: {age}')
-age += 5
-print(f'New Age: {age}')
-is_student = True
-print(f'Is Student: {is_student}')
