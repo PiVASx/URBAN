e538115b3b7bf9097a4509879a4b0056b0d6f520
glpat-BgjeT-FSL-bEXXenhGCN <sergeevvasia85@gmail.com>
2024-10-04 16:28:26 +0300

    Решение module_9_3

--- module_9/module_9_3.py
+++ module_9/module_9_3.py
@@ -2,7 +2,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 
-first_result = (len(x[0]) - len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+first_result = (len(x) - len(z) for x, z in zip(first, second) if len(x) != len(z))
 
 second_result = (len(first[index]) == len(second[index]) for index in range(len(first)))
 
