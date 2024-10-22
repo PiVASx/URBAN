6306b6d0a3f2b96d7dfaa6aff788a4982141c3e5
glpat-BgjeT-FSL-bEXXenhGCN <sergeevvasia85@gmail.com>
2024-10-22 19:12:27 +0300

    Решение module_10_3

--- module_10/module_10_3.py
+++ module_10/module_10_3.py
@@ -1,12 +1,16 @@
-
+from threading import Thread, Lock
+import time
 
 class Bank:
-    def __init__(self, balance, lock):
+    def __init__(self, balance):
         self.balance = balance
-        self.lock = lock
+        self.lock = Lock()
 
-    def deposit(self, num):
-        if 50 >= num <= 500:
-            self.balance += num
-
+    def deposit(self):
+        num = range(50, 501)
+        if self.balance >= 500 and self.lock.locked():
+            self.lock.release()
+        self.balance += num
+        print(f"Пополнение: {num}. Баланс: {self.balance}")
+        time.sleep(0.001)
 
