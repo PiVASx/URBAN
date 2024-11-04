6c80b3247c2bebf1f4e9158e31ca671cb5b995ec
PiVAS <sergeevvasa85@gmal.com>
2024-11-04 13:18:15 +0300

    Исправление модуля 10_3

--- module_10/module_10_3.py
+++ module_10/module_10_3.py
@@ -1,16 +1,53 @@
+from random import randint
 from threading import Thread, Lock
 import time
 
+
 class Bank:
-    def __init__(self, balance):
-        self.balance = balance
+    def __init__(self):
+        self.balance = 0
         self.lock = Lock()
 
     def deposit(self):
-        num = range(50, 501)
-        if self.balance >= 500 and self.lock.locked():
-            self.lock.release()
-        self.balance += num
-        print(f"Пополнение: {num}. Баланс: {self.balance}")
-        time.sleep(0.001)
+        i = 0
+        while i < 100:
+            num = randint(50, 501)
+            self.lock.acquire()  # Пытаемся захватить блокировку
+            try:
+                self.balance += num
+                print(f"Пополнение: {num}. Баланс: {self.balance}")
+            finally:
+                self.lock.release()  # Освобождаем блокировку
+            i += 1
+            time.sleep(0.001)
 
+    def take(self):
+        i = 0
+        while i < 100:
+            num = randint(50, 501)
+            print(f'Запрос на {num}')
+            self.lock.acquire()  # Пытаемся захватить блокировку
+            try:
+                if num <= self.balance:
+                    self.balance -= num
+                    print(f"Снятие: {num}. Баланс: {self.balance}")
+                else:
+                    print(f'Запрос отклонён, недостаточно средств.')
+            finally:
+                self.lock.release()  # Освобождаем блокировку
+            i += 1
+            time.sleep(0.001)
+
+
+bk = Bank()
+
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
