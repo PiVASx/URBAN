754ffb666921d1fe55c9a8e10a36ebbcb6d2b434
glpat-BgjeT-FSL-bEXXenhGCN <sergeevvasia85@gmail.com>
2024-11-05 19:13:56 +0300

    Дополнение module_10_4

--- module_10/module_10_4.py
+++ module_10/module_10_4.py
@@ -1,12 +1,14 @@
 from random import randint
 from threading import Thread, Lock
 from time import sleep
+from queue import Queue
 
 
 class Table:
     def __init__(self, number):
         self.number = number
         self.guest = None
+
 
 class Guest(Thread):
     def __init__(self, name):
@@ -18,4 +20,13 @@ class Guest(Thread):
 
 
 class Cafe:
-    pass
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = tables
+
+    def guest_arrival(self, guest):
+        self.queue.put(guest)
+        print(f"Гость {guest} встал в очередь.")
+
+    def discuss_guests(self):
+        pass
